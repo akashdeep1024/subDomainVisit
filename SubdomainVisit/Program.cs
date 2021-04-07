0ca6170500d925b23efb137ee2d40d872fe80bc9
Akash Deep <akashdeep1024@gmail.com>
2021-04-06 22:47:24 -0600

    added subdomain visit

---
+++ SubdomainVisit/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SubdomainVisit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] domainString = { "9001 discuss.leetcode.com", "1 yahoo.com" };
+
+            Dictionary<string, int> dictDomain = new Dictionary<string, int>();
+
+            foreach (var domItem in domainString)
+            {
+                string[] allDomain = domItem.Split();
+
+                string[] domainValues = allDomain[1].Split(".");
+                int count = Convert.ToInt32(allDomain[0]);
+                string currentDomain = "";
+
+                for (int i = domainValues.Length - 1; i >= 0; i--)
+                {
+                    currentDomain = domainValues[i] + (i < domainValues.Length - 1 ? "." : "") + currentDomain;
+
+                    if (dictDomain.ContainsKey(currentDomain))
+                    {
+                        dictDomain[currentDomain] += count;
+                    }
+                    else
+                    {
+                        dictDomain.Add(currentDomain, count);
+                    }
+
+                }
+            }
+
+            List<string> list = new List<string>();
+            foreach (var d in dictDomain)
+            {
+                list.Add(" " + d.Key + " " + d.Value);
+            }
+
+            foreach (var l in list)
+            {
+                Console.WriteLine(l);
+            }
+
+        }
+    }
+}
